e6ef6733a8b5f4b4735009743d6a2dde5121a703
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2025-02-28 16:35:14 +0300

    updated the socket.io and add typing

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import User from "../models/user.model.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -26,6 +27,22 @@ io.on("connection", (socket) => {
 
   // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("typing", async ({ userId }) => {
+    try {
+        
+
+        socket.broadcast.emit("userTyping", { userId });
+    } catch (error) {
+        console.error("Error finding user:", error);
+    }
+});
+
+
+  // Handle stop typing event
+  socket.on("stopTyping", ({ userId }) => {
+    socket.broadcast.emit("userStoppedTyping");
+  });
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
